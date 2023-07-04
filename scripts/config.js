4499979a4a5d15744aac27736f0b9ebb7eaa1ed0
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2023-07-04 22:55:05 +0700

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "GANTI DENGAN PROJECT KAMU",
-  authDomain: "GANTI DENGAN PROJECT KAMU",
-  databaseURL: "GANTI DENGAN PROJECT KAMU",
-  projectId: "GANTI DENGAN PROJECT KAMU",
-  storageBucket: "GANTI DENGAN PROJECT KAMU",
-  messagingSenderId: "GANTI DENGAN PROJECT KAMU",
-  appId: "GANTI DENGAN PROJECT KAMU"
+    apiKey: "AIzaSyBcTsdNM48Ts1t5JTNdDby8281e7s7REDE",
+    authDomain: "simforum-login.firebaseapp.com",
+    databaseURL: "https://simforum-login-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "simforum-login",
+    storageBucket: "simforum-login.appspot.com",
+    messagingSenderId: "311465979099",
+    appId: "1:311465979099:web:8ded10990bee13bfb770fe",
+    measurementId: "G-WP0XZT3CHT"
 };
 const app = firebase.initializeApp(firebaseConfig);
 const rdb = firebase.database();
