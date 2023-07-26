6d75c5c24a4b0301fc9f83ccfd951c0c1bdf14e4
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2023-07-26 12:52:06 +0700

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBcTsdNM48Ts1t5JTNdDby8281e7s7REDE",
-    authDomain: "simforum-login.firebaseapp.com",
-    databaseURL: "https://simforum-login-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "simforum-login",
-    storageBucket: "simforum-login.appspot.com",
-    messagingSenderId: "311465979099",
-    appId: "1:311465979099:web:8ded10990bee13bfb770fe",
-    measurementId: "G-WP0XZT3CHT"
+apiKey: "AIzaSyC-SEma3JShAJXe4h47x9nWPMRf8aF9e7M", authDomain: "tokko-login.firebaseapp.com", databaseURL: "https://tokko-login-default-rtdb.firebaseio.com", projectId: "tokko-login", storageBucket: "tokko-login.appspot.com", messagingSenderId: "604336992026", appId: "1:604336992026:web:8091ec5a0b8a1a5d15c6a3", measurementId: "G-VYVZGNYLC3"
 };
 const app = firebase.initializeApp(firebaseConfig);
 const rdb = firebase.database();
